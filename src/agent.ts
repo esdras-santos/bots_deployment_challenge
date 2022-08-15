15a3ab3292ba9a67e92918c5926d2f5525ec679a
esdras-santos <esantoz@protonmail.com>
2022-08-15 09:56:36 -0300

    Need Review

---
+++ src/agent.ts
@@ -0,0 +1,51 @@
+import {
+  Finding,
+  HandleTransaction,
+  TransactionEvent,
+  FindingSeverity,
+  FindingType,
+} from "forta-agent"
+
+import {NETHERMIND_BOT_DEPLOYER, AGENTS_PROXY, FUNCTION_SIGNATURE} from './utils'
+
+
+export function provideHandleTransaction(deployer: string, proxy: string, signature: string): HandleTransaction{
+  return async (txEvent: TransactionEvent): Promise<Finding[]> => {
+    const findings: Finding[] = []
+
+    if(txEvent.from != deployer.toLowerCase() || txEvent.to != proxy.toLowerCase()){
+      return findings
+    }
+
+    const createdAgentCalls = txEvent.filterFunction(
+      signature,
+      proxy 
+    )
+
+    createdAgentCalls.forEach((call) =>{
+      const {agentId, metadata, chainIds} = call.args
+
+      findings.push(
+        Finding.fromObject({
+          name: "Deployed Bot",
+          description:"New Bot Deployed",
+          alertId: "NETHERMIND-FORTA-BOT",
+          severity: FindingSeverity.Info,
+          type: FindingType.Info,
+          protocol: "nethermind-forta-deployer",
+          metadata: {
+            agentId: agentId.toString(),
+            metadata,
+            chainIds: chainIds.toString()
+          }
+        })
+      )
+    })
+    
+    return findings
+  }
+}
+
+export default {
+  handleTransaction: provideHandleTransaction(NETHERMIND_BOT_DEPLOYER, AGENTS_PROXY, FUNCTION_SIGNATURE),
+};

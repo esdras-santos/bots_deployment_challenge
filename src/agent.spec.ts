00d3aca194ccdf7530f7bac4372e62f4926f2a46
esdras-santos <esantoz@protonmail.com>
2022-08-24 12:13:54 -0300

    Need Review

--- src/agent.spec.ts
+++ src/agent.spec.ts
@@ -7,8 +7,29 @@ import { BOTS_PROXY, FUNCTION_ABI, NETHERMIND_BOT_DEPLOYER } from "./utils";
 
 describe("Bots deployment", () => {
   let proxy = new Interface([FUNCTION_ABI]);
-
   let handleTransaction: HandleTransaction;
+
+  type mockMetadata = {
+    agentId: string,
+    metadata: string,
+    chainIds: string,
+  }
+
+  let mockFinding = (metadata: mockMetadata): Finding => {
+    return Finding.fromObject({
+      name: "Deployed Bot",
+      description: "New Bot Deployed",
+      alertId: "NETHERMIND-FORTA-BOT",
+      severity: FindingSeverity.Info,
+      type: FindingType.Info,
+      protocol: "nethermind-forta-deployer",
+      metadata: {
+        agentId: metadata.agentId,
+        metadata: metadata.metadata,
+        chainIds: metadata.chainIds,
+      },
+    })
+  }
 
   beforeAll(() => {
     handleTransaction = provideHandleTransaction(NETHERMIND_BOT_DEPLOYER, BOTS_PROXY, FUNCTION_ABI);
@@ -43,6 +64,51 @@ describe("Bots deployment", () => {
     expect(findings).toStrictEqual([]);
   });
 
+  it("return and detect multiple calls to createAgent", async () => {
+    let findings: Finding[];
+    let txEvent: TestTransactionEvent;
+    txEvent = new TestTransactionEvent()
+      .setTo(BOTS_PROXY)
+      .setFrom(NETHERMIND_BOT_DEPLOYER)
+      .addTraces({
+        to: BOTS_PROXY,
+        from: NETHERMIND_BOT_DEPLOYER,
+        function: proxy.getFunction("createAgent"),
+        arguments: [
+          123456,
+          "0x2477d97E71F7738d48e86aF5a107616F71F43263",
+          "QmXGC4eWXjShzXs9T3nUvrYTTPqLQCEfArHfuSLSCagE2E",
+          [56],
+        ],
+      })
+      .addTraces({
+        to: BOTS_PROXY,
+        from: NETHERMIND_BOT_DEPLOYER,
+        function: proxy.getFunction("createAgent"),
+        arguments: [
+          12345678,
+          "0x2477d97E71F7738d48e86aF5a107616F71F43263",
+          "QmXGC4eWXjShzXs9T3nUvrYTTPqLQCEfArHfuSLSCagE2E",
+          [56],
+        ],
+      })
+
+    findings = await handleTransaction(txEvent);
+
+    let metadata1: mockMetadata = {
+      agentId: "123456",
+      metadata: "QmXGC4eWXjShzXs9T3nUvrYTTPqLQCEfArHfuSLSCagE2E",
+      chainIds: "56"
+    }
+
+    let metadata2: mockMetadata = {
+      agentId: "12345678",
+      metadata: "QmXGC4eWXjShzXs9T3nUvrYTTPqLQCEfArHfuSLSCagE2E",
+      chainIds: "56"
+    }
+    expect(findings).toStrictEqual([mockFinding(metadata1), mockFinding(metadata2)]);
+  })
+
   it("returns the finding when bot is created", async () => {
     let findings: Finding[];
     let txEvent: TestTransactionEvent;
@@ -61,20 +127,12 @@ describe("Bots deployment", () => {
       });
 
     findings = await handleTransaction(txEvent);
-    expect(findings).toStrictEqual([
-      Finding.fromObject({
-        name: "Deployed Bot",
-        description: "New Bot Deployed",
-        alertId: "NETHERMIND-FORTA-BOT",
-        severity: FindingSeverity.Info,
-        type: FindingType.Info,
-        protocol: "nethermind-forta-deployer",
-        metadata: {
-          agentId: "123456",
-          metadata: "QmXGC4eWXjShzXs9T3nUvrYTTPqLQCEfArHfuSLSCagE2E",
-          chainIds: "56",
-        },
-      }),
-    ]);
+
+    let metadata: mockMetadata = {
+      agentId: "123456",
+      metadata: "QmXGC4eWXjShzXs9T3nUvrYTTPqLQCEfArHfuSLSCagE2E",
+      chainIds: "56"
+    }
+    expect(findings).toStrictEqual([mockFinding(metadata)]);
   });
 });
